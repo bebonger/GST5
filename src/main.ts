f54d5f23194cf5c5ff56430437338b29a64851d9
VizioJoash <113297969+VizioJoash@users.noreply.github.com>
2023-04-27 07:11:22 +0800

    changed api call conditions

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ import VueAxios from 'vue-axios';
 import type { User } from './stores/userData'
 import { useUserDataStore } from './stores/userData'
 
+import { inject } from 'vue'
+
 import './assets/main.css'
 
 const app = createApp(App)
@@ -20,28 +22,5 @@ app.use(router)
 app.use(VueCookies);
 app.use(VueAxios, axios);
 app.provide('axios', app.config.globalProperties.axios);
-
-const response = await axios.get('/api/get-user', { withCredentials: true })
-
-const user : User = {
-    avatar_url: response.data.avatar_url,
-    country_code: response.data.country_code,
-    default_group: response.data.default_group,
-    id: response.data.id,
-    is_active: response.data.is_active,
-    is_bot: response.data.is_bot,
-    is_deleted: response.data.is_deleted,
-    username: response.data.username,
-    is_restricted: response.data.is_restricted,
-    global_rank: response.data.global_rank,
-    country_rank: response.data.country_rank,
-    badges: response.data.badges
-}
-
-console.log(user);
-
-const userDataStore = useUserDataStore()
-userDataStore.SetUser(user)
-console.log(userDataStore.IsLoggedIn)
 
 app.mount('#app')
