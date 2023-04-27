58e178f43560718bf78a6ac03e40e6adedcb7385
VizioJoash <113297969+VizioJoash@users.noreply.github.com>
2023-04-27 19:18:50 +0800

    moved GET user-data to main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,4 +23,31 @@ app.use(VueCookies);
 app.use(VueAxios, axios);
 app.provide('axios', app.config.globalProperties.axios);
 
+try {
+axios.get('/api/get-user', { withCredentials: true }).then((response: { data: any }) => {
+
+    const user : User = {
+        avatar_url: response.data.avatar_url,
+        country_code: response.data.country_code,
+        default_group: response.data.default_group,
+        id: response.data.id,
+        is_active: response.data.is_active,
+        is_bot: response.data.is_bot,
+        is_deleted: response.data.is_deleted,
+        username: response.data.username,
+        is_restricted: response.data.is_restricted,
+        global_rank: response.data.global_rank,
+        country_rank: response.data.country_rank,
+        badges: response.data.badges
+    }
+
+    console.log(user);
+    
+    const userDataStore = useUserDataStore()
+    userDataStore.SetUser(user)
+    console.log(userDataStore.IsLoggedIn)
+})}
+catch(err) {
+    console.log(err);
+}
 app.mount('#app')
